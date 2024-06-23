9dbb2b3e46b91cf5d47d663a03a719981aea0921
Joel <joel.ph.dunham@gmail.com>
2024-06-22 23:59:14 -0400

    Specializing to ESP32 and cleanup

--- src/ESP2SOTA.cpp
+++ src/ESP2SOTA.cpp
@@ -1,43 +1,57 @@
 #include "ESP2SOTA.h"
 #include "index_html.h"
-//Class constructor
-ESP2SOTAClass::ESP2SOTAClass(){
+
+// Class constructor
+ESP2SOTAClass::ESP2SOTAClass()
+{
 
 }
 
-#if defined(ESP8266)
-  void ESP2SOTAClass::begin(ESP8266WebServer *server){
-#elif defined(ESP32)
-  void ESP2SOTAClass::begin(WebServer *server){
-#endif
+void ESP2SOTAClass::begin(WebServer *server)
+{
   _server = server;
-  //Returns index.html page
-  _server->on("/update", HTTP_GET, [&]() {
+  // Returns index.html page
+  _server->on("/update", HTTP_GET, [&]()
+  {
     _server->sendHeader("Connection", "close");
     _server->send(200, "text/html", indexHtml);
   });
 
-  /*handling uploading firmware file */
-  _server->on("/update", HTTP_POST, [&]() {
+  // Handling uploading firmware file
+  _server->on("/update", HTTP_POST, [&]()
+  {
     _server->sendHeader("Connection", "close");
     _server->send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
     ESP.restart();
-  }, [&]() {
+  }, [&]()
+  {
     HTTPUpload& upload = _server->upload();
-    if (upload.status == UPLOAD_FILE_START) {
+    if (upload.status == UPLOAD_FILE_START)
+    {
       Serial.printf("Update: %s\n", upload.filename.c_str());
-      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+      if (!Update.begin(UPDATE_SIZE_UNKNOWN))
+      {
+        // Start with max available size
         Update.printError(Serial);
       }
-    } else if (upload.status == UPLOAD_FILE_WRITE) {
-      /* flashing firmware to ESP*/
-      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+    }
+    else if (upload.status == UPLOAD_FILE_WRITE)
+    {
+      // Flashing firmware to ESP
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize)
+      {
         Update.printError(Serial);
       }
-    } else if (upload.status == UPLOAD_FILE_END) {
-      if (Update.end(true)) { //true to set the size to the current progress
+    }
+    else if (upload.status == UPLOAD_FILE_END)
+    {
+      if (Update.end(true))
+      {
+        // True to set the size to the current progress
         Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-      } else {
+      }
+      else
+      {
         Update.printError(Serial);
       }
     }
---
+++ src/ota_definitions.h
@@ -0,0 +1,13 @@
+#ifndef OTA_DEFINITIONS_H
+#define OTA_DEFINITIONS_H
+
+#include <WiFi.h>
+#include <WiFiAP.h>
+#include <WebServer.h>
+/* INCLUDE ESP2SOTA LIBRARY */
+#include <ESP2SOTA.h>
+
+const char* ssid = "ESP2SOTA";
+const char* password = "softrobot";
+
+#endif
